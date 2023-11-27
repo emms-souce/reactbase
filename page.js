89a6867f1f63235e5e11aab428c176a24d5ce12b
emms-souce <85787734+emms-souce@users.noreply.github.com>
2023-11-27 13:39:00 +0100

    Add files via upload

---
+++ page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NouveauCV from '../components/NouveauCV';
+
+const Cv = () => {
+  return (
+    <div>
+      <h1>CV</h1>
+      <NouveauCV title="Super titre !" />
+    </div>
+  );
+}
+
+export default Cv;
